51511bfbc2c89bb36bbd3887c4c2490be41cb8bd
PaulSweeney89 <g00229879@gmit.ie>
2020-02-09 12:39:58 +0000

    Cleaned up comments in code & updated output to reflect problem sheet example output

--- collatz.py
+++ collatz.py
@@ -4,23 +4,20 @@
 # if input value is odd (multiple input value by 3 & add 1)
 # end program if current value = 1
 
-# input must be a interger number or program will not run
 x = int(input("Input positive interger number: "))
 
-# run 'while loop' until condition is met
+print(int(x), end=' ') # print input valued as part of output & output all on same line
+
 while x != 1:
 
-# conditional statement 1 - input number must be positive & greater than 0 for program to continue
     if x <= 0:
-        print(x, "is not a positive interger number.")
+        print(int(x), "is not a positive interger number.")
         break
 
-# conditional statement 2- if input number is even execute below formula 
     elif x % 2 == 0:
         x = x / 2
-        print(x)
-
- # conditional statement 3- if input number is odd execute below formula    
-    elif x % 2 != 0:
+        print(int(x), end=' ') # print output all on same line
+  
+    else:
         x = x * 3 + 1
-        print(x)
+        print(int(x), end=' ') # print output all on same line
